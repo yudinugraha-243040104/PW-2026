eeb4a26c4f6ca0d5786651bbb7d20f74234893a2
unknown <yudi.nugraha@unpas.ac.id>
2026-02-12 22:03:07 +0700

    rumah

---
+++ Pertemuan1/Rumah.php
@@ -0,0 +1,19 @@
+<?php
+
+class Rumah {
+    public $warna
+    Public $alamat
+
+
+    public function __construct ($$warna,$$alamat ) {
+
+    $this warna = $warna
+    $this alamat = $alamat
+    }
+}
+
+function pasangLIstrik (rumah $dataRumah){
+    return "Listrik sedang dipasang di rumah " . $dataRumah>
+}
+//Update 10.46
+?>
